96c64393fed8d3844d1d9f9f154fcf305a7b6df9
taimoor139 <taimoorahmad139@gmail.com>
2021-08-26 14:57:48 +0500

    Second

--- index.php
+++ index.php
@@ -36,7 +36,9 @@
         }else{
             echo "mail not sent!";
         }
-        
+        error_reporting(-1);
+        ini_set('display_errors', 'On');
+        set_error_handler("var_dump");
 
             
 }
---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+  
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+  
+require 'vendor/autoload.php';
+  
+$mail = new PHPMailer(true);
+  
+try {
+    $mail->setFrom('taimoormughal304@gmail.com', 'Name');           
+    $mail->addAddress('taimoorahmad139@gmail.com');
+       
+    $mail->isHTML(true);                                  
+    $mail->Subject = 'Subject';
+    $mail->Body    = 'HTML message body in <b>bold</b> ';
+    $mail->AltBody = 'Body in plain text for non-HTML mail clients';
+    $mail->send();
+    echo "Mail has been sent successfully!";
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+  
+?>
